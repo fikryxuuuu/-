b0033aa30fe2e6f1a3a69aabeefb4ffdc4b7c772
fikryxuuuu <90820840+fikryxuuuu@users.noreply.github.com>
2026-08-09 15:21:28 +0800

    Update WebHomeSpider.java

--- com/github/catvod/spider/WebHomeSpider.java
+++ com/github/catvod/spider/WebHomeSpider.java
@@ -1,1 +1,59 @@
 
+package com.github.catvod.spider;
+
+import com.fongmi.core.spider.Spider;
+import android.content.Context;
+
+public class WebHomeSpider extends Spider {
+    private String webUrl;
+
+    @Override
+    public void init(Context context, String extend) {
+        webUrl = extractHomePage(extend);
+    }
+
+    private String extractHomePage(String json) {
+        if (json == null || json.isEmpty()) return "";
+        try {
+            int idx = json.indexOf("homePage");
+            if (idx == -1) return "";
+            idx = json.indexOf(':', idx);
+            if (idx == -1) return "";
+            idx = json.indexOf('"', idx);
+            if (idx == -1) return "";
+            int end = json.indexOf('"', idx + 1);
+            if (end == -1) return "";
+            return json.substring(idx + 1, end);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    @Override
+    public String homeContent(boolean filter) {
+        if (webUrl == null || webUrl.isEmpty()) {
+            return "{}";
+        }
+        return "{\"webUrl\":\"" + webUrl + "\"}";
+    }
+
+    @Override
+    public String categoryContent(String tid, boolean filter, String extend) {
+        return "{}";
+    }
+
+    @Override
+    public String detailContent(String[] ids) {
+        return "{}";
+    }
+
+    @Override
+    public String searchContent(String key, boolean quick, String extend) {
+        return "{}";
+    }
+
+    @Override
+    public String playerContent(String flag, String id, String[] vipFlags) {
+        return "{}";
+    }
+}
